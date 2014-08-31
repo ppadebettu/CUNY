068aac74b9a4ca8263fd0d3589af924f5210241f
ppadebettu <prashanth.padebettu@spsmail.cuny.edu>
2014-08-31 11:22:44 -0400

    Week 1 Assignment Answers

---
+++ week_1_assignment.R
@@ -0,0 +1,6 @@
+# Answers to Week 1 Assignment Questions
+# Student Name:       Prashanth Padebettu
+# R Version:          3.1.1
+# RStudio Version:    0.98.1028
+# PostgreSQL Version: 9.3
+# 'sales' dataset in {DMwR} package contains 401146 obversations of 5 variables
